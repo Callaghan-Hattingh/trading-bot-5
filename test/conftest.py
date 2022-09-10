ee638979ff3e7930701deca3fdb63eae7d941aaf
Callaghan <callaghan@metalmanagementsolutions.com>
2022-09-10 15:00:17 +0200

    basic testing set, with basic model tests

---
+++ test/conftest.py
@@ -0,0 +1,19 @@
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+from src.db.base import Base
+
+engine = create_engine("sqlite:///", echo=False)
+TestingSessionLocal = scoped_session(sessionmaker(bind=engine))
+
+
+@pytest.fixture()
+def session():
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
---
+++ test/test_models/test_order.py
@@ -0,0 +1,11 @@
+from src.models import Order, OrderFactory
+
+
+def test_order(session):
+    q = session.query(Order).all()
+    assert q == []
+    OrderFactory.create()
+    q = session.query(Order).all()
+    assert q != []
+
+
