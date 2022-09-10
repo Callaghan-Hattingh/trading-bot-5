88a8b37342d67662ba017f623a02cf2aee00ef6a
Callaghan <callaghan@metalmanagementsolutions.com>
2022-09-10 18:24:57 +0200

    added schemas for the input data

---
+++ src/schemas/trade.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+
+
+class TradeModel(BaseModel):
+    price = float
+    quantity = float
+    currencyPair = str
+    tradedAt = str
+    takerSide = str
+    sequenceId = int
+    id = str
+    quoteVolume = float
