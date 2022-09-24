4e488025dfb3c582ece05a91fd4e9f9bd2a4ef21
Callaghan <callaghan@metalmanagementsolutions.com>
2022-09-24 20:31:09 +0200

    added post_lot_gen and batch_lot_gen

--- src/logic/lot/lot.py
+++ src/logic/lot/lot.py
@@ -1,4 +1,11 @@
-from src.core.config import c_id, step
+from src.core.config import (
+    c_id,
+    step,
+    currency_pair,
+    quantity,
+    post_only,
+    time_in_force,
+)
 from src.logic.api import ValrApi
 
 
@@ -6,11 +13,33 @@ def gen_customer_order_id(origin_price: int, currency_pair: str) -> str:
     return f"{c_id}{currency_pair}{origin_price}"
 
 
-def filtered_open_orders(curency_pair: str) -> set:
-    r = ValrApi.get_all_open_orders()
-    print(r)
-    return set(r)
+def filtered_open_orders(*, curency_pair: str = currency_pair, c_id: str = c_id) -> set:
+    oo = ValrApi.get_all_open_orders()
+    if not oo:
+        return set()
+    else:
+        raise Exception
 
 
+def post_lot_generation(price: int, *, side: str) -> dict:
+    i = gen_customer_order_id(price, currency_pair)
+    data = {
+        "side": side.upper(),
+        "quantity": quantity,
+        "price": price,
+        "pair": currency_pair,
+        "postOnly": post_only,
+        "customerOrderId": i,
+        "timeInForce": time_in_force,
+    }
+    return data
 
 
+def batch_lot_generation(
+    data: list[dict], *, order_type: str
+) -> list[dict[str, str | dict]]:
+    reqs = []
+    for _ in data:
+        i = {"type": order_type, "data": _}
+        reqs.append(i)
+    return reqs
